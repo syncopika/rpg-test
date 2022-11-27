77c03ecaedecd4f32a1677eb018dff2670c0f58c
nicholas hung <nhung93@outlook.com>
2022-11-27 13:30:50 -0500

    adding some controllers for the biplane and cardboard box. also added more boxes to the main scene.

---
+++ Assets/scripts/tool controllers/BiplaneController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BiplaneController : MonoBehaviour
+{
+    bool playerInRange = false;
+
+    public GameObject propeller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.name.Equals("low-poly-human-edit-rig2-edit"))
+        {
+            propeller.transform.Rotate(new Vector3(0, 0, 1) * 500f);
+        }
+    }
+}
---
+++ Assets/scripts/tool controllers/BoxController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = transform.GetComponent<Animator>();
+        animator.SetBool("isOpen", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // close the box
+        if (other.name.Equals("low-poly-human-edit-rig2-edit"))
+        {
+            animator.SetBool("isOpen", false);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // open the box
+        if (other.name.Equals("low-poly-human-edit-rig2-edit"))
+        {
+            animator.SetBool("isOpen", true);
+        }
+    }
+}
