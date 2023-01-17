a91e73fbf1a1840950a767fbc054ddda515a13a2
nicholas hung <nhung93@outlook.com>
2023-01-16 23:19:41 -0500

    more controllers for npcs

---
+++ Assets/scripts/FarmerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmerController : MonoBehaviour
+{
+    private Quaternion initialRot;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialRot = transform.rotation;
+        animator = transform.GetComponent<Animator>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.transform.name.Contains("low-poly-human"))
+        {
+            transform.LookAt(other.transform);
+
+            animator.SetBool("isIdle", true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        animator.SetBool("isIdle", false);
+        transform.rotation = initialRot;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/NpcController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcController : MonoBehaviour
+{
+    public GameObject armRight;
+    public GameObject footLeft;
+    public GameObject footRight;
+    public float maxRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        armRight.transform.Rotate(new Vector3(1, 0, 0), armRight.transform.rotation.eulerAngles.x + 65 - (maxRotation * Mathf.Cos(Time.time * 3)));
+        footLeft.transform.Rotate(new Vector3(0, 0, 1),  Mathf.Cos(Time.time * 5));
+    }
+}
